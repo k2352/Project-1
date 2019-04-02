18f98dd946a494f658428050b93e9e652e0c940a
K H <k2yru@outlook.com>
2019-04-02 23:26:52 +0000

    Lab 6 assignment work (03/04/2019)

--- main.c
+++ main.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
 
+char task1(char message, int key);
+
 int main()
 {
-  return 0;
+    printf("Please select an option: \n");
+    printf("a) Task 1\n");
+    printf("b) Task 2\n");
+    printf("c) Task 3\n");
+    printf("Selection: ");
+    
+    char c = 'a';
+   //scanf("%c", &c);
+    
+    do
+    {
+        switch(c)
+        {
+              case 'a':
+              {
+                  char message[100];
+                  int key;
+                  printf("Enter a message to be encrypted: ");
+                  scanf("%s", &message);
+                  printf("Enter the key: ");
+                  scanf("%d", &key);
+                  task1(message, key);
+                  break;
+              }
+             
+              //break;
+              //case 'b': task2();
+              //break;
+              //case 'c': task3();
+              //break;
+              default: printf("Unknown option %c\n Please enter a, b, c or d\n", c);   
+        }
+
+    }
+    while (c < 'a' || c > 'd');
+    return 0;   
 }
+
+char task1(char message, int key)
+{
+    //char encrypt[];
+    int i;
+    
+    for (i=0; (i<100 && message[i] != '\0'; i++)
+    {
+        message[i] = message[i] + key;
+    }
+    
+    //for (i=0; message[i] != '\0'; i++)
+    //{
+      //  encrypt = message[i];
+        
+        //if (encrypt >= 'a' && encrypt <= 'z')
+        //{
+          //  encrypt = encrypt + key;
+        //}  
+    //}
+    printf("Encrypted message is: %s", message);
+
+    return 0;
+}
+
