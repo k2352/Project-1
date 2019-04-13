d23262db178d766a8a8667e223701ad8e66fa452
K H <k2yru@outlook.com>
2019-04-13 08:10:02 +0000

    work

--- main.c
+++ main.c
@@ -13,10 +13,12 @@
 #include <stdlib.h>
 #include <string.h>
 char task1(int key);  //Function prototype for task1 function
-char task2(char* message, int key, int n);  //Function prototype for task2 function
-char task5(char* message, int n);           //Function prototype for task5 function
+char task2(int key);  //Function prototype for task2 function
+char task5(FILE *input_b, FILE *output_b);  //Function prototype for task5 function
 FILE *input_a;
 FILE *output_a;
+FILE *input_b;
+FILE *output_b;
 
 
 int main()
@@ -28,7 +30,7 @@ int main()
     printf("e) Task 5\n");
     printf("Selection: ");
     
-    char c = 'a';
+    char c = 'e';
    //scanf("%c", &c);
     
     do
@@ -37,23 +39,7 @@ int main()
         {
             case 'a':   //Encrypt a message with a rotation cipher
             {
-                //char message[100];
-                //char message[100] = "hello world!";
-                int key, i, n;
-                //printf("Enter a message to be encrypted: ");
-                //scanf("%s", message);
-                //for (i=0; i<100; i++)   //Determine how many characters are in the array
-                //{
-                  //  if (message[i] != '\0')
-                    //{
-                      //  n++;
-                    //}
-                    //else
-                    //{
-                      //  break;
-                    //}
-                  
-                //}
+                int key;
                 printf("Enter the key: ");
                 scanf("%d", &key);
                 while ((key<0) || (key>26))
@@ -66,24 +52,7 @@ int main()
             }
             case 'b':   //Decrypt an encrypted message with a rotation cipher
             {
-                //char message[100];
-                char message[100] = "ifmmp xpsme!";
-                //char message[] = "SJSFMPCRM WG O USBWIG. PIH WT MCI XIRUS O TWGV PM WHG OPWZWHM HC QZWAP O HFSS, WH KWZZ ZWJS WHG KVCZS ZWTS PSZWSJWBU HVOH WH WG GHIDWR. - OZPSFH SWBGHSWB";
-               
-                int key, i, n;
-                //printf("Enter a message to be decrypted: ");
-                //scanf("%s", message);
-                for (i=0; i<100; i++)   //Determine how many characters are in the array
-                {
-                    if (message[i] != '\0')
-                    {
-                        n++;
-                    }
-                    else
-                    {
-                        break;
-                    }  
-                }
+                int key;
                 printf("Enter the key: ");
                 scanf("%d", &key);
                 while ((key<0) || (key>26))
@@ -91,28 +60,12 @@ int main()
                     printf("Invalid key. Please try again.");
                     scanf("%d", &key);
                 }
-                task2(message, key, n);
+                task2(key);
                 break;
             }
             case 'e':
             {
-                //char message[100];
-                char message[100] = "TVU TVAOTH: AOL KHAH IYVBNOA AV";
-                int i, n;
-                //printf("Enter a message to be decrypted: ");
-                //scanf("%s", message);
-                for (i=0; i<100; i++)   //Determine how many characters are in the array
-                {
-                    if (message[i] != '\0')
-                    {
-                        n++;
-                    }
-                    else
-                    {
-                        break;
-                    }  
-                }
-                task5(message, n);
+                task5(input_b, output_b);
                 break;
             }
             default: printf("Unknown option %c\n Please enter a, b, c or d\n", c);   
@@ -123,8 +76,9 @@ int main()
     return 0;   
 }
 
-//Function which takes a message, key and number of characters in an array as input
-//and returns the message encrypted
+//Function which takes a key as input, reads a message from a file to encrypt,
+//prints that encrypted message to the console, and also writes the encrypted 
+//message to a new file
 char task1(int key)
 {
     input_a = fopen("input_a.txt", "r");
@@ -161,273 +115,143 @@ char task1(int key)
     return 0;
 }
 
-//Function which takes an encrypted message, key and number of characters in an array as input
-//and returns the message decrypted
-char task2(char* message, int key, int n)
+//Function which takes a key as input, reads an encrypted message from a file to decrypt,
+//prints that decrypted message to the console, and also writes the decrypted 
+//message to a new file
+char task2(int key)
 {
-    int i;
-    
-    for (i=0; i<n; i++)
+    input_b = fopen("input_b.txt", "r");
+    output_b = fopen("output_b.txt", "w");
+    if(input_b == NULL) 
     {
-        if (message[i]>=97 && message[i]<=122)
+        perror("fopen()");
+        return 0;
+    }
+    char c;
+    while(fscanf(input_b, "%c", &c) == 1)
+    {
+        if (c>=97 && c<=122)
         {
-            message[i] = message[i]-97;
-            if ((message[i]-key)<0)
+            c = c-97;
+            if ((c-key)<0)
             {
-                message[i] = (message[i]-key+26)%26;
+                c = (c-key+26)%26;
             }
             else
             {
-                message[i] = (message[i] - key)%26;   
+                c = (c - key)%26;   
             }
-            message[i] = message[i]+65;
+            c = c+65;
         }
         
-        else if (message[i]>=65 && message[i]<=90)
+        else if (c>=65 && c<=90)
         {
-            message[i] = message[i]-65;
-            if ((message[i]-key)<0)
+            c = c-65;
+            if ((c-key)<0)
             {
-                message[i] = (message[i]-key+26)%26;
+                c = (c-key+26)%26;
             }
             else
             {
-                message[i] = (message[i] - key)%26;   
+                c = (c - key)%26;   
             }
-            message[i] = message[i]+65;
+            c = c+65;
         }
-        
         else
         {
-            message[i] = message[i];
+            c = c;
         }
+        fprintf(output_b, "%c", c); //Write character to output file
+        printf("%c", c);    //Print character to console
     }
-    printf("Decrypted message is: %s\n", message);
+    fclose(input_b);
+    fclose(output_b);
     return 0;
 }
 
 //Function which takes an encrypted message and number of characters in an array as input
 //and returns the message decrypted without knowledge of the key
-char task5(char* message, int n)
+char task5(FILE *input_b, FILE *output_b)
 {
-    int i, key, k, compare;
-    char c[10] = "THE";
+    input_b = fopen("input_b.txt", "r");
+    output_b = fopen("output_b.txt", "w");
+    if(input_b == NULL) 
+    {
+        perror("fopen()");
+        return 0;
+    }
+    char c;
+    cha *message;
+    char word[10] = "THE";
+    //char message[200];
     char *result;
+    int k;
     
     for (k=7; k<8; k++)
     {
-        for (i=0; i<n; i++)
+        while(fscanf(input_b, "%c", &c) == 1)
         {
-            if (message[i]>=97 && message[i]<=122)
+            if (c>=97 && c<=122)
             {
-                message[i] = message[i]-97;
-                if ((message[i]-k)<0)
+                c = c-97;
+                if ((c-k)<0)
                 {
-                    message[i] = (message[i]-k+26)%26;
+                    c = (c-k+26)%26;
                 }
                 else
                 {
-                    message[i] = (message[i] - k)%26;   
+                    c = (c - k)%26;   
                 }
-                message[i] = message[i]+65;
+                c = c+65;
             }
-            else if (message[i]>=65 && message[i]<=90)
+            else if (c>=65 && c<=90)
             {
-                message[i] = message[i]-65;
-                if ((message[i]-k)<0)
+                c = c-65;
+                if ((c-k)<0)
                 {
-                    message[i] = (message[i]-k+26)%26;
+                    c = (c-k+26)%26;
                 }
                 else
                 {
-                    message[i] = (message[i] - k)%26;   
+                    c = (c - k)%26;   
                 }
-                message[i] = message[i]+65;
+                c = c+65;
             }
             else
             {
-                message[i] = message[i];
+                c = c;
             }
+            printf("%c", c);    //Tester
+            
         }
-        //printf("%s\n", message);
-        result = strstr(message, c);
+        //char message[200] = c;
+        //for (int i=0; i<200; i++)
+        //{
+          //  message[i] = c;
+        //}
+        //printf("%s", message);
+        message = &c;
+        
+        result = strstr(message, word);
         if (result != NULL)
         {
-            //printf("%s", result);
-            //printf("it worked");
-            //key=k;
-            //printf("%d", key);
-            
-           // for (i=0; i<n; i++)
-            //{
-              //  if (message[i]>=97 && message[i]<=122)
-                //{
-                  //  message[i] = message[i]-97;
-                    //message[i] = (message[i] - k)%26;
-                   // message[i] = message[i]+65;
-                //}/
-                //e/lse if (message[i]>=65 && message[i]<=90)
-                //{
-                  //  message[i] = message[i]-65;
-                    //message[i] = (message[i] - k)%26;
-                    //message[i] = message[i]+65;
-                //}
-                //else
-                //{
-                  //  message[i] = message[i];
-                //}
-           // }
-            printf("Decrypted message is: %s\n", message);
+            fprintf(output_b, "%c", c); //Write character to output file
+            printf("%c", c);    //Print character to console
             break;
         }
+
+            //result = strstr(c, word);
+            //if (result != NULL)
+            //{
+              //  fprintf(output_b, "%c", c); //Write character to output file
+                //printf("%c", c);    //Print character to console
+                //break;
+
+            //}
+        //} 
     }
-
-        //else
-        ///{
-        //	printf("didnt work");
-		//}    }
-
-        //printf("\nDecrypted message is: %s\n", message);
-        //while(!feof(checkWord))
-        //{
-          //  fscanf(checkWord, "%c", c);
-            //printf("%c", *c);
-            //char str1[] = "arbor";
-            //if(strstr(c, str1) != NULL)
-            //
-               // printf("yes");
-            //}
-            //if (strstr(message, c) != NULL)
-            //{
-                //printf("yes\n");
-                //break;
-            //}
-            //else
-            //{
-              //  printf("yes");
-                //break;
-            //}
-            
-            
-            //compare = strcmp(message, c);
-            //printf("\nDecrypted message is: %s\n", message);
-            //if (compare==0)
-            //{
-              //  key=k;
-                //printf("%d\n", key);
-                //printf("\nDecrypted message is: %s\n", message);
-                //break;
-            //}
-            //else
-            //{
-              //  printf("error");
-                //break;
-            //}
-
-            //printf("%d\n", compare);
-            
-            
-        
-        //{
-          //  char c;
-            //fscanf(checkWord, "%c", &c);
-            //printf("%c", c);
-        //}
-        //fscanf(checkWord, "%s", strg);
-        //printf("%s\n", strg);
-        //printf("\nDecrypted message is: %s\n", message);
-    
-    
-    
-    //checkWord = fopen("words.txt", "r");
-    //if(checkWord == NULL) 
-    //{
-      //  perror("fopen()");
-        //return 0;
-    //}
-  //  int i, key, k, compare;
-    //const char c[10] = "the";
-    //char *result;
-    
-    //for (k=0; k<1; k++)
-    //{
-      //  for (i=0; i<n; i++)
-        //{
-          //  if (message[i]>=97 && message[i]<=122)
-            //{
-              //  message[i] = message[i]-97;
-                //message[i] = (message[i] - k)%26;
-                //message[i] = message[i]+97;
-            //}
-            //else if (message[i]>=65 &&3 message[i]<=90)
-            //{
-              //  message[i] = message[i]-65;
-                //message[i] = (message[i] - k)%26;
-                //message[i] = message[i]+97;
-            //}
-            //else
-            //{
-              //  message[i] = message[i];
-            //}
-        //}
-        //result = strstr(message, c);
-        //if (result != NULL)
-        //{
-          //  printf("%s", result);
-        //}
-//    }
-
-        //printf("\nDecrypted message is: %s\n", message);
-        //while(!feof(checkWord))
-        //{
-          //  fscanf(checkWord, "%c", c);
-            //printf("%c", *c);
-            //char str1[] = "arbor";
-            //if(strstr(c, str1) != NULL)
-            //
-               // printf("yes");
-            //}
-            //if (strstr(message, c) != NULL)
-            //{
-                //printf("yes\n");
-                //break;
-            //}
-            //else
-            //{
-              //  printf("yes");
-                //break;
-            //}
-            
-            
-            //compare = strcmp(message, c);
-            //printf("\nDecrypted message is: %s\n", message);
-            //if (compare==0)
-            //{
-              //  key=k;
-                //printf("%d\n", key);
-                //printf("\nDecrypted message is: %s\n", message);
-                //break;
-            //}
-            //else
-            //{
-              //  printf("error");
-                //break;
-            //}
-
-            //printf("%d\n", compare);
-            
-            
-        
-        //{
-          //  char c;
-            //fscanf(checkWord, "%c", &c);
-            //printf("%c", c);
-        //}
-        //fscanf(checkWord, "%s", strg);
-        //printf("%s\n", strg);
-        //printf("\nDecrypted message is: %s\n", message);
-        
-        
-   // }
+    fclose(input_b);
+    fclose(output_b);
     return 0;
 }
+
